af3c49c93d88cd229a065c7c8ed09f6742782409
Gabriel Trzimajewski <95551770+Sn0wye@users.noreply.github.com>
2026-05-14 19:18:02 -0300

    feat: add QuestionDto and AnswerDto

---
+++ API/Dto/QuestionDto.cs
@@ -0,0 +1,20 @@
+using API.Entities;
+
+namespace API.Dto;
+
+public class QuestionDto
+{
+    public int Id { get; set; }
+    public string Text { get; set; }
+    public string[] Images { get; set; }
+    public QuestionType Type { get; set; }
+    public int SelectCount { get; set; }
+    public ICollection<AnswerDto> Answers { get; set; }
+}
+
+public class AnswerDto
+{
+    public int Id { get; set; }
+    public string? Text { get; set; }
+    public string? Image { get; set; }
+}
