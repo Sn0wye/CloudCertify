7a04b76739f3636e1f5e72020e06a65c2244f0fa
Gabriel Trzimajewski <95551770+Sn0wye@users.noreply.github.com>
2026-05-14 19:19:52 -0300

    fix: make QuestionDto.Text nullable and initialize Answers collection

--- API/Dto/QuestionDto.cs
+++ API/Dto/QuestionDto.cs
@@ -5,11 +5,11 @@ namespace API.Dto;
 public class QuestionDto
 {
     public int Id { get; set; }
-    public string Text { get; set; }
+    public string? Text { get; set; }
     public string[] Images { get; set; }
     public QuestionType Type { get; set; }
     public int SelectCount { get; set; }
-    public ICollection<AnswerDto> Answers { get; set; }
+    public ICollection<AnswerDto> Answers { get; set; } = new List<AnswerDto>();
 }
 
 public class AnswerDto
