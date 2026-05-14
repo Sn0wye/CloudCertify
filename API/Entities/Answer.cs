9d0757d890c25c85a1e7803cd2b7dc1cac805f35
Gabriel Trzimajewski <95551770+Sn0wye@users.noreply.github.com>
2026-05-14 15:15:39 -0300

    feat: make Answer.Text nullable and add Image field

--- API/Entities/Answer.cs
+++ API/Entities/Answer.cs
@@ -8,16 +8,18 @@ public class Answer
 {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; init; }
-    
+
     public int QuestionId { get; set; }
-    
-    public string Text { get; set; }
-    
+
+    public string? Text { get; set; }
+
     [JsonIgnore]
     public bool IsCorrect { get; set; }
-    
+
+    public string? Image { get; set; }
+
     public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
-    
+
     [JsonIgnore]
-    public virtual Question Question { get; set; } 
+    public virtual Question Question { get; set; }
 }
