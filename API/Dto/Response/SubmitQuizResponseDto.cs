aea2314c04a1b60132b1b3dc1ffb57751ecfe2b6
Gabriel Trzimajewski <95551770+Sn0wye@users.noreply.github.com>
2026-05-14 19:25:12 -0300

    feat: add metadata and Image to submit response DTOs

--- API/Dto/Response/SubmitQuizResponseDto.cs
+++ API/Dto/Response/SubmitQuizResponseDto.cs
@@ -15,13 +15,19 @@ public class QuizResultQuestionDto
     public required int Id { get; set; }
     public required string Text { get; set; }
     public required QuestionType Type { get; set; }
+    public string? Domain { get; set; }
+    public string[]? Concepts { get; set; }
+    public string? ServiceCategory { get; set; }
+    public string[]? Services { get; set; }
+    public string? Explanation { get; set; }
     public required List<QuizResultAnswerDto> Answers { get; set; }
 }
 
 public class QuizResultAnswerDto
 {
     public required int Id { get; set; }
-    public required string Text { get; set; }
+    public required string? Text { get; set; }
+    public required string? Image { get; set; }
     public required bool IsCorrect { get; set; }
     public required bool WasSelected { get; set; }
 }
